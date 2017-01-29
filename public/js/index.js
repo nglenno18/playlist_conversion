c5fa80c4253bcebe3cb61836eb851098fd85cdce
nglenno18 <nglenno18@gmail.com>
2017-01-29 13:26:34 -0600

    dynamically adjust width of bottom body

--- public/js/index.js
+++ public/js/index.js
@@ -6,13 +6,12 @@ socket.on('connect', function(){
 
 
 jQuery('.section').on('click', function(e){
-  console.log(e);
   var sectionid = e.currentTarget.id;
   console.log('\n\nID is: ', e.currentTarget.id);
   resetSections();
   if($('#bottom_body_container').css("visibility")=="collapse"){
     if($(e.currentTarget).css("margin-top")=="0px") {
-      console.log('Bottom plane was collapsed, should expand');
+      // console.log('Bottom plane was collapsed, should expand');
       var color = $(e.currentTarget).css("background-color");
       console.log('\nCOLOR: ', color);
 
@@ -32,14 +31,58 @@ jQuery('.section').on('click', function(e){
 
 });
 
+jQuery('.section').hover(function(e){
+  var sectionid = e.currentTarget;
+  console.log(sectionid);
+
+  if($('#bottom_body_container').css("visibility")!="collapse" &&
+  sectionid.id !="section4"){
+
+    $(e.currentTarget).addClass("section-spacing");
+  }
+  if(sectionid.id=="section4"){
+    $('#section4').css("margin-left", "5px");
+  }else{
+    // $('#section4').css("margin-left","5px");
+  }
+
+}, function(e){
+  resetSpacing();
+  $('#section4').css("margin-left","5.7px");
+});
+
+jQuery('body').on('mouseover', function(){
+  var width = $('#body_width').width();
+  width = width - 19;
+  var strwidth = "" + width + "px";
+  console.log('\n\n\nWIDTH IS :', strwidth);
+  $('#bottom_body').css("width", strwidth);
+  width = 1037 - width;
+  console.log('\n\n\nWIDTH IS :', width);
+
+  $('#bottom_body').css("border-right-width", width);
+
+})
+
 var resetSections = function(){
   console.log('RESETTING ALL SECTIONS');
   $('#section1').removeClass("highlighted-section");
+  $('#section1').removeClass("section-spacing");
   $('#section1').addClass("section");
   $('#section2').removeClass("highlighted-section");
+  $('#section2').removeClass("section-spacing");
   $('#section2').addClass("section");
   $('#section3').removeClass("highlighted-section");
+  $('#section3').removeClass("section-spacing");
   $('#section3').addClass("section");
   $('#section4').removeClass("highlighted-section");
+  $('#section4').removeClass("section-spacing");
   $('#section4').addClass("section");
+}
+var resetSpacing = function(){
+  console.log('RESETTING spacing');
+  $('#section1').removeClass("section-spacing");
+  $('#section2').removeClass("section-spacing");
+  $('#section3').removeClass("section-spacing");
+  $('#section4').removeClass("section-spacing");
 }
